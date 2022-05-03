ae145ca97182c861a14af838cd4d2ae5a226092e
Tisana Roongsawang <tisana@gmail.com>
2022-05-03 17:16:44 +0700

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import Game from './Game';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Game />);
+
 registerServiceWorker();
